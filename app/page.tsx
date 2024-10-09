2d8790111a7a2e83f6a5a0fc70fc67cea58383db
Jarren San Jose <sanjose.jarren@gmail.com>
2024-10-08 23:15:13 -0700

    add latte tax definition / explanation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">latte tax</h1>
+        <p className="text-2xl">noun</p>
+        <p>The latte tax is the price to pay for flow state.</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
