53aed4b7cce3707bd6770f99fb5ece33a35132cd
Jarren San Jose <jarrensj@users.noreply.github.com>
2024-10-12 20:35:18 -0700

    update latte tax definition

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="text-5xl font-bold">latte tax</h1>
         <p className="text-xl italic text-gray-600">noun</p>
         <p className="text-lg text-justify leading-relaxed">
-          The <span className="font-bold">latte tax</span> is the price to pay for an iced latte, even if you can make a better one at home, because the routine or break of going to a coffee shop helps offer perspective or focus, preventing bad habits and bad trading days or helping with productivity.
+          The <span className="font-bold">latte tax</span> is the price to pay for an iced latte, even if you have spent a stupid amount on coffee equipment for at home or have fresh beans, because the routine or break of going to a coffee shop helps offer perspective or focus, preventing bad habits and bad trading days or helping with productivity.
         </p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
